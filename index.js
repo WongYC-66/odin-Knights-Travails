3903d5dfc7661fa3bea6ea84addc4d1ec2308923
WongYC-66 <yengchien@hotmail.com>
2023-08-08 18:32:09 +0800

    with simple UI

--- index.js
+++ index.js
@@ -1,120 +1,89 @@
-const MAX_X = 7
-const MAX_Y = 7
-const MIN_X = 0
-const MIN_Y = 0
+const MAX_ROW = 7
+const MAX_COL = 7
+const MIN_ROW = 0
+const MIN_COL = 0
 
-const Game = function () {
-    this.board = []
-    for (let y = MAX_Y; y >= 0; y--) {
-        let row = []
-        for (let x = 0; x <= MAX_X; x++) {
-            let newNode = new Node(x, y)
-            row.push(newNode)
-        }
-        this.board.push(row)
+class Board {
+    constructor(root = null) {
+        this.board = [...Array(8)].map(x => Array(8).fill(' '))
     }
 }
 
-const Knight = function (root = null) {
-    // let newNode = new Node(0, 0)
-    this.path = []
-    this.root = root
-
+class Knight {
+    constructor(x, y, parent = null) {
+        this.x = x
+        this.y = y
+        this.parent = parent
+    }
 }
 
-const Node = function (x = null, y = null, next = [], visited = false, distance = 0) {
-    this.x = x
-    this.y = y
-    this.next = next
-    this.visited = visited
-    this.distance = distance
-    // this.moves = moves
-    // this.count = count
-}
-
-
-function createMoves(game) {
-    // board , each location is a node, pointing to other node. graph relationship
-    let count = 0
-    game.board.forEach((row, i) => {
-        row.forEach((node, j) => {
-            count++
-            // Find out all possible move, a knight can have 8 next moves (at max)
-            if (node.x - 2 >= MIN_X && node.y - 1 >= MIN_Y) node.next.push([node.x - 2, node.y - 1])
-            if (node.x - 1 >= MIN_X && node.y - 2 >= MIN_Y) node.next.push([node.x - 1, node.y - 2])
-            if (node.x + 1 <= MAX_X && node.y - 2 >= MIN_Y) node.next.push([node.x + 1, node.y - 2])
-            if (node.x + 2 <= MAX_X && node.y - 1 >= MIN_Y) node.next.push([node.x + 2, node.y - 1])
-            if (node.x - 2 >= MIN_X && node.y + 1 <= MAX_Y) node.next.push([node.x - 2, node.y + 1])
-            if (node.x - 1 >= MIN_X && node.y + 2 <= MAX_Y) node.next.push([node.x - 1, node.y + 2])
-            if (node.x + 1 <= MAX_X && node.y + 2 <= MAX_Y) node.next.push([node.x + 1, node.y + 2])
-            if (node.x + 2 <= MAX_X && node.y + 1 <= MAX_Y) node.next.push([node.x + 2, node.y + 1])
-            //
-            // console.log(node.next)
-            node.next = node.next.map(item => {
-                let boardCoord = convertToBoardCoord(item)
-                // console.log(boardCoord)
-                let otherNode = game.board[boardCoord[0]][boardCoord[1]]
-                return otherNode
-            })
-            // console.log(count)
+// BFS
+function knightMoves(start, end) {
+    let newNode = new Knight(start[0], start[1])
+    let queue = [newNode]
+    let solution
+    while (queue.length > 0) {
+        let parent = queue.shift() // if use queue.pop(), is DFS
+        let grandPa = parent.parent
+        if (parent.x === end[0] && parent.y === end[1]) {
+            solution = parent // found then out
+            break;
+        }
+        // add 8 possible moves
+        let possibleMovesOfParent = []
+        let move1 = new Knight(parent.x - 2, parent.y - 1, parent)
+        let move2 = new Knight(parent.x - 1, parent.y - 2, parent)
+        let move3 = new Knight(parent.x + 1, parent.y - 2, parent)
+        let move4 = new Knight(parent.x + 2, parent.y - 1, parent)
+        let move5 = new Knight(parent.x + 2, parent.y + 1, parent)
+        let move6 = new Knight(parent.x + 1, parent.y + 2, parent)
+        let move7 = new Knight(parent.x - 1, parent.y + 2, parent)
+        let move8 = new Knight(parent.x - 2, parent.y + 1, parent)
+        possibleMovesOfParent.push(move1, move2, move3, move4, move5, move6, move7, move8)
+        possibleMovesOfParent = possibleMovesOfParent.filter(node => {
+            if (node.x < MIN_COL || node.x > MAX_COL) return false // out of boundary, no need
+            if (node.y < MIN_ROW || node.y > MAX_ROW) return false // out of boundary, no need
+            if (grandPa && node.x === grandPa.x && node.y === grandPa.y) return false // cannot revert to grandPa. 
+            let [rowIdx, colIdx] = coordConversion([node.x, node.y])
+            // if (board.board[rowIdx][colIdx] === 'x') return false // repeated, no need // maybe for DFS?
+            return true
         })
-    })
-
-}
-
-// try with BFS
-function knightMoves(start, end, game) {
-    // console.table({start, end})
-    start = convertToBoardCoord(start)
-    end = convertToBoardCoord(end)
-    let startNode = game.board[start[0]][start[1]]
-    let endNode = game.board[end[0]][end[1]]
-    let solution = bfs(startNode, endNode)
-    console.log(solution)
-
-}
-
-// https://www.techiedelight.com/chess-knight-problem-find-shortest-path-source-destination/
-function bfs(x, y) {
-    let newKnight = new Knight(x)
-    let visited = new Set()
-    console.log(newKnight)
-    let queue = [x]
-    let currentNode = newKnight.root
-    let parent = newKnight.root
-    while(queue.length > 0){
-        console.log(queue)
-        currentNode = queue.shift()
-        if(visited.has(currentNode)) continue
-        currentNode.distance = parent.distance + 1
-        if(currentNode === y){
-            return currentNode.distance
-        } else {
-            visited.add(currentNode)
-            currentNode.next.forEach(x => queue.push(x))
-        }
-        parent = currentNode
+        possibleMovesOfParent.forEach(node => {
+            let [rowIdx, colIdx] = coordConversion([node.x, node.y])
+            // board.board[rowIdx][colIdx] = 'x' // marking, maybe for DFS ?
+        })
+        queue = [...queue, ...possibleMovesOfParent]
+        // console.log(queue)
+        // console.log(board)
     }
-    console.log('out of whileloop')
+    console.log('solution found')
+    let solutionArr = []
+    let tmp = solution
+    solutionArr.unshift([tmp.x, tmp.y])
+    while (tmp.parent) {
+        tmp = tmp.parent
+        solutionArr.unshift([tmp.x, tmp.y])
+    }
+    //draw to chess board
+    let finishedBoard = new Board()
+    solutionArr.forEach(coord => {
+        let [rowIdx, colIdx] = coordConversion(coord)
+        finishedBoard.board[rowIdx][colIdx] = 'x'
+    })
+    console.log('----------------------------------------------------')
+    console.log(`knightMoves([${start}], [${end}])`)
+    console.log(`You made it in ${solutionArr.length - 1} moves! Here's your path:`)
+    solutionArr.forEach(coord => console.log(`[${coord[0]}, ${coord[1]}]`))
+    console.table(finishedBoard.board)
+    console.log(solution)
+    console.log('----------------------------------------------------')
 }
 
-
-function convertToBoardCoord(arr) {
-    //helper function
-    let boardCoord = [MAX_Y - arr[1], arr[0]] // row-7,col-0
-    return boardCoord
+function coordConversion(coord) {
+    return [MAX_ROW - coord[1], coord[0]] // tested
 }
 
-
-
-// test
-let newGame = new Game()
-// let newKnight = new Knight()
-createMoves(newGame)
-
-// console.log(newGame.board)
-// console.log(newKnight)
-
-
-// console.log(knightMoves([0, 0], [1, 2], newGame))
-console.log(knightMoves([0, 0], [3, 3], newGame))
+// knightMoves([0, 0], [1, 2])
+// knightMoves([0, 0], [3, 3])
+// knightMoves([3, 3], [0, 0])
+// knightMoves([3, 3], [4, 3])
--- index_dom.js
+++ index_dom.js
@@ -1,25 +1,24 @@
-const MAX_ROW = 7
-const MAX_COL = 7
-const MIN_ROW = 0
-const MIN_COL = 0
+// const MAX_ROW = 7
+// const MAX_COL = 7
+// const MIN_ROW = 0
+// const MIN_COL = 0
 
-class Board {
-    constructor(root = null) {
-        this.board = [...Array(8)].map(x => Array(8).fill(' '))
-    }
-}
+// class Board {
+//     constructor(root = null) {
+//         this.board = [...Array(8)].map(x => Array(8).fill(' '))
+//     }
+// }
 
-class Knight {
-    constructor(x, y, parent = null) {
-        this.x = x
-        this.y = y
-        this.parent = parent
-    }
-}
-
+// class Knight {
+//     constructor(x, y, parent = null) {
+//         this.x = x
+//         this.y = y
+//         this.parent = parent
+//     }
+// }
 
 // BFS
-function knightMoves(start, end, board = new Board()) {
+function knightMovesDOM(start, end) {
     let newNode = new Knight(start[0], start[1])
     let queue = [newNode]
     let solution
@@ -29,7 +28,7 @@ function knightMoves(start, end, board = new Board()) {
         if (parent.x === end[0] && parent.y === end[1]) {
             solution = parent // found then out
             break;
-        } 
+        }
         // add 8 possible moves
         let possibleMovesOfParent = []
         let move1 = new Knight(parent.x - 2, parent.y - 1, parent)
@@ -44,24 +43,20 @@ function knightMoves(start, end, board = new Board()) {
         possibleMovesOfParent = possibleMovesOfParent.filter(node => {
             if (node.x < MIN_COL || node.x > MAX_COL) return false // out of boundary, no need
             if (node.y < MIN_ROW || node.y > MAX_ROW) return false // out of boundary, no need
-            if (grandPa && node.x === grandPa.x && node.y ===grandPa.y) return false // cannot revert to grandPa. 
+            if (grandPa && node.x === grandPa.x && node.y === grandPa.y) return false // cannot revert to grandPa. 
             let [rowIdx, colIdx] = coordConversion([node.x, node.y])
-            // if (board.board[rowIdx][colIdx] === 'x') return false // repeated, no need // maybe for DFS?
             return true
         })
         possibleMovesOfParent.forEach(node => {
             let [rowIdx, colIdx] = coordConversion([node.x, node.y])
-            // board.board[rowIdx][colIdx] = 'x' // marking, maybe for DFS ?
         })
         queue = [...queue, ...possibleMovesOfParent]
-        // console.log(queue)
-        // console.log(board)
     }
     console.log('solution found')
     let solutionArr = []
     let tmp = solution
     solutionArr.unshift([tmp.x, tmp.y])
-    while(tmp.parent){
+    while (tmp.parent) {
         tmp = tmp.parent
         solutionArr.unshift([tmp.x, tmp.y])
     }
@@ -69,22 +64,54 @@ function knightMoves(start, end, board = new Board()) {
     let finishedBoard = new Board()
     solutionArr.forEach(coord => {
         let [rowIdx, colIdx] = coordConversion(coord)
-        finishedBoard.board[rowIdx][colIdx] = 'x' 
+        finishedBoard.board[rowIdx][colIdx] = 'x'
     })
     console.log('----------------------------------------------------')
-    console.log(`knightMoves(${start}, ${end})`)
+    console.log(`knightMoves([${start}], [${end}])`)
     console.log(`You made it in ${solutionArr.length - 1} moves! Here's your path:`)
     solutionArr.forEach(coord => console.log(`[${coord[0]}, ${coord[1]}]`))
     console.table(finishedBoard.board)
     console.log(solution)
     console.log('----------------------------------------------------')
+
+    // DOM version
+    let str = ''
+    str += '----------------------------------------------'
+    str += '</br>'
+    str += `knightMoves([${start}], [${end}])`
+    str += '</br>'
+    str += `You made it in ${solutionArr.length - 1} moves! Here's your path:`
+    str += '</br>'
+    solutionArr.forEach(coord => str += `[${coord[0]}, ${coord[1]}]  </br>`)
+
+    let display = document.querySelector('.display')
+    display.innerHTML = str
+
+    // draw board
+    let boardDisplay = document.createElement('div')
+    boardDisplay.classList.add('board')
+    display.append(boardDisplay)
+    finishedBoard.board.forEach(row => {
+        row.forEach(cell => {
+            const el = document.createElement('div')
+            el.textContent = cell
+            boardDisplay.append(el)
+        })
+    })
+
+
 }
 
 function coordConversion(coord) {
     return [MAX_ROW - coord[1], coord[0]] // tested
 }
 
-knightMoves([0, 0], [1, 2])
-knightMoves([0, 0], [3, 3])
-knightMoves([3, 3], [0, 0])
-knightMoves([3, 3], [4, 3])
+// DOM version, UI/input/draw board
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#run').addEventListener('click', () => {
+        let start = document.querySelector('#startLoc').value.split(',').map(x => Number(x))
+        let end = document.querySelector('#endLoc').value.split(',').map(x => Number(x))
+        console.log(start, end)
+        knightMovesDOM(start, end)
+    })
+})
